1b6a24dc1064b16ff993dfbddc16952d8c6691a3
Yassine <yassine753357@gmail.com>
2017-05-04 14:23:53 +0200

    InitialProjetc

---
+++ Albaque_2/Albaque_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Albaque_2.Startup))]
+namespace Albaque_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
